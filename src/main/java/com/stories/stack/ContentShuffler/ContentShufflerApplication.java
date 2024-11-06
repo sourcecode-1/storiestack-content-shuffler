b89ffe61e0fab63f7265f1bef249a4e951513a3f
sourcecode-1 <developer4810@gmail.com>
2024-11-05 21:23:17 -0500

    Adding Scheduling Annotation

--- src/main/java/com/stories/stack/ContentShuffler/ContentShufflerApplication.java
+++ src/main/java/com/stories/stack/ContentShuffler/ContentShufflerApplication.java
@@ -2,13 +2,12 @@ package com.stories.stack.ContentShuffler;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class ContentShufflerApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(ContentShufflerApplication.class, args);
 	}
-
 }
